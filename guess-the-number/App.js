729e996cdab82d63a42996c0b11b68c392d11da7
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2020-01-26 18:44:11 -0300

    Added Header component (guess-the-number).

--- guess-the-number/App.js
+++ guess-the-number/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View />
+    <View style={styles.screen}>
+      <Header style={styles.header} title="Guess a number" />
+      <View style={styles.content}>
+        <Text>Content!</Text>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({ });
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  },
+  header: {
+    height: '20%'
+  },
+  content: {
+    height: '90%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ guess-the-number/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = (props) => {
+  const { title } = props;
+
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: '#f7c1c1',
+    height: 100
+  },
+  headerTitle: {
+    fontSize: 30,
+    color: 'white'
+  }
+});
+
+export default Header;
